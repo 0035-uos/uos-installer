1445a1cb625545d027ab46990a6663f753515595
zhangbingyi <zhangbingyi@uniontech.com>
2022-05-18 17:19:22 +0800

    update

--- libuos-installer/utils/utils.h
+++ libuos-installer/utils/utils.h
@@ -6,8 +6,8 @@
 
 namespace Tools {
 
-static const QString parted_parameter_path = "/uos-installer/parted.json";
-static const QString parted_sys_info_path = "/uos-installer/installer-settings.json";
+static const QString parted_parameter_path = "/installer/parted.json";
+static const QString parted_sys_info_path = "/installer/installer-settings.json";
 
 
 enum Installation{WTI_Default = 0, WTI_Local, WTI_SerialPort, WTI_Socket, WTI_PXE, WTI_AutoInstall};
