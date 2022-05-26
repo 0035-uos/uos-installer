866585ec09c338f3960d6fc29dc7ba0788c20755
zhangbingyi <zhangbingyi@uniontech.com>
2022-05-26 12:04:33 +0800

    update

--- uos-installer/systeminfoconfig.cpp
+++ uos-installer/systeminfoconfig.cpp
@@ -17,10 +17,36 @@ void SystemInfoConfig::initData()
 void SystemInfoConfig::run()
 {
     // 这里可以增加用户自定义设置
-    GJsonItem user_item;
-    user_item.appendValue("username", "deepin");
-    user_item.appendValue("password", "deepin12!");
-    m_data->appendItem("user", &user_item);
+    {
+        GJsonItem user_item;
+        user_item.appendValue("username", "deepin");
+        user_item.appendValue("password", "deepin12!");
+        user_item.appendValue("rootpassword", "deepin12!");
+        user_item.appendValue("hostname", "deepin-pc");
+        m_data->appendItem("user", &user_item);
+    }
+    {
+        GJsonItem item;
+        item.appendValue("xkblayout", "");
+        item.appendValue("xkbvariant", "");
+        item.appendValue("xkboptions", "");
+        item.appendValue("xkbmodel", "");
+        m_data->appendItem("keyboard", &item);
+    }
+    {
+        GJsonItem item;
+        item.appendValue("locale", "");
+        m_data->appendItem("language", &item);
+    }
+    {
+        GJsonItem item;
+        item.appendValue("DI_TIMEZONE", "Asia/Shanghai");
+        item.appendValue("DI_IS_LOCAL_TIME", "true");
+        item.appendValue("DI_IS_ENABLE_NTP", "true");
+        item.appendValue("SET_DATE_TIME", "2022-05-17 15:39:13");
+        item.appendValue("timezone_is_sysnc", "true");
+        m_data->appendItem("timezone", &item);
+    }
     m_data->commitData(); // 数据确认后，提交修改
 }
 
